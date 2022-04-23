09fd178571710e56755854ee1022bb75f2c57843
Avishek <avishekd653@gmail.com>
2022-04-23 16:02:56 +0530

    unkwon file changes here too

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
 import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
+// import ProductList from "./pages/ProductList";
 // import Register from "./pages/Register";
 // import Login from "./pages/Login";
 // import Cart from "./pages/Cart";
